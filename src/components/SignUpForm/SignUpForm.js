adec122f7432f8b545169416a6c377ff45c3d4c4
Glendon Chin <glendonchin@gmail.com>
2022-06-01 16:48:17 -0400

    create sign up form

---
+++ src/components/SignUpForm/SignUpForm.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import "./SignUpForm.css"
+const SignUpForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    return (
+        <div className="container">
+            <form onSubmit={(event)=> {
+                event.preventDefault()
+                alert(email+' '+password)}}>
+                <label>Email</label>
+                <input 
+                    type="email" 
+                    value={email} 
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+                <label>Password</label>
+                <input 
+                    type="password"
+                    value={password}  
+                    onChange={(event) => setPassword(event.target.value)}
+                ></input>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUpForm;
