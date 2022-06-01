5d55b31a6e86f7e37fec27ada3228046620e6db4
Glendon Chin <glendonchin@gmail.com>
2022-06-01 13:18:36 -0400

    create react app and basic components

---
+++ src/Text/Text.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Text.css"
+
+const Text = () => {
+    return (
+        <div className='Text'>
+            <h1>Looking for music?</h1>
+            <p>Start listening now.</p>
+        </div>
+    )
+};
+
+export default Text;
